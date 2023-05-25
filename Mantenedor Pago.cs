cb2c9a760d85d71806f61c565c80e2e44b4dda5d
Nazareth <halo.12.12@hotmail.com>
2023-05-24 21:16:28 -0500

    Agregar archivos de proyecto.

---
+++ Mantenedor Pago.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModelamientoDeSoftware
+{
+    public partial class Mantenedor_Pago : Form
+    {
+        public Mantenedor_Pago()
+        {
+            InitializeComponent();
+        }
+    }
+}
